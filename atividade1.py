77488b617ba14852f10baadee9ea459df174798b
Lucas Ferreira <Lucasmaiaferreira73@gmail.com>
2022-04-09 15:32:29 -0300

    Update atividade1.py

--- atividade1.py
+++ atividade1.py
@@ -1,13 +1,15 @@
 for n in range(4):
     #Solicita o código do estado da carga
     CodigoEstado = int(input("Digite o código do estado da carga: "))
+
+    #Solicita o peso da carga em toneladas:
     PesoCargaToneladas = int(input("Peso da carga do caminhão em toneladas: "))
 
+    #Aqui já converteu o peso de toneladas em quilos:
     PesoCargaQuilos = PesoCargaToneladas * 1000
-    #Aqui já converteu o peso de toneladas em quilos
 
+    #Solicita o código da carga:
     CodigoCarga = int(input("Digite o código da carga entre 10 e 40: "))
-    #Aqui ocorre a solicitação do código da carga
     
     #<-- Aqui vai começar as condições dependendo do codigo da carga -->
     if(CodigoCarga >= 10 and CodigoCarga <= 20): #Se "CodigoCarga" inserido for entre 10 e 20, faça:
