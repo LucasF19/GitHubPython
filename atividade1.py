ca1068703b13e815adbef60de46f2201b597e351
Lucas Ferreira <Lucasmaiaferreira73@gmail.com>
2022-04-12 11:42:26 -0300

    encurtei

--- atividade1.py
+++ atividade1.py
@@ -7,42 +7,46 @@ for n in range(4):
 
     #Aqui já converteu o peso de toneladas em quilos:
     PesoCargaQuilos = PesoCargaToneladas * 1000
-
     #Solicita o código da carga:
     CodigoCarga = int(input("Digite o código da carga entre 10 e 40: "))
     
     #<-- Aqui vai começar as condições dependendo do codigo da carga -->
+    def Carga(n2):
+        PrecoKg = PesoCargaQuilos * n2
+        return PrecoKg
+
     if(CodigoCarga >= 10 and CodigoCarga <= 20): #Se "CodigoCarga" inserido for entre 10 e 20, faça:
-        PrecoKg = PesoCargaQuilos * 100
+        cod = Carga(100)
 
     elif(CodigoCarga >= 21 and CodigoCarga <= 30):#Se "CódigoCarga" inserido for entre 21 e 30, faca:
-        PrecoKg = PesoCargaQuilos * 250
+        cod = Carga(250)
 
     elif(CodigoCarga >= 31 and CodigoCarga <= 40):#Se "CodigoCarga" inserido for entre 31 e 40, faça:
-        PrecoKg = PesoCargaQuilos * 340
-    else:#Se não for nenhuma das condições anteriores, então o código será invalidado
+        cod = Carga(340)
+    else: #Se não for nenhuma das condições anteriores, então o código será invalidado
         print("Código invalido!")
 
-    # <-- Função para calcular o imposto, o total e exibir o resultado total -->
-    def impostoEtotal(imp):
-        Imposto = PrecoKg * imp
-        Total = (PrecoKg + Imposto)
-
-        print(f'Peso da carga em Kg:{PesoCargaQuilos} \nPreço da carga do caminhão: R${PrecoKg} \nImposto: R${Imposto} \nPreço carga total{Total}')
+    # <-- Função para calcular o imposto -->
+    def imposto(imp):
+        Imposto = cod * imp
+        return Imposto
 
     # <-- Condição para verificar o "CodigoEstado" da carga -->
     if CodigoEstado == 1: #Se "CodigoEstado" for igual a 1, faça:
-        impostoEtotal(0.35)
+        Imp = imposto(0.35)
     elif CodigoEstado == 2: #Se "CodigoEstado" for igual a 2, faça:
-        impostoEtotal(0.25)
+        Imp = imposto(0.25)
     elif CodigoEstado == 3: #Se "CodigoEstado" for igual a 3, faça:
-        impostoEtotal(0.15)
+        Imp = imposto(0.15)
     elif CodigoEstado == 4: #Se "CodigoEstado" for igual a 4, faça:
-        impostoEtotal(0.05)
+        Imp = imposto(0.05)
     elif CodigoEstado == 5: #Se "CodigoEstado" for igual a 5, faça:
-        impostoEtotal(0.0)
+        Imp = imposto(0.0)
     else: #Caso o "CodigoEstado" não for nenhuma das anteriores ele será invalidado
         print("Código de estado inválido")
 
+    Total = cod + Imp
     
+    print(f'Peso da carga em Kg:{PesoCargaQuilos} \nPreço da carga do caminhão: R${cod} \nImposto: R${Imp} \nPreço carga total: R${Total}')
 
+    
